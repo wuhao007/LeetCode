1153542b2dcf644690cf20b2a333c769c2abe5ec
Hao Wu <tohaowu@gmail.com>
2014-12-30 14:21:54 -0500

    Inspire me, Dijkstra

---
+++ move-zero.py
@@ -0,0 +1,9 @@
+def moveZero(A):
+    l = 0
+    for i in range(len(A)):
+        if A[i] != 0:
+            A[l], A[i] = A[i], A[l]
+            l += 1
+    print A
+    return len(A) - l
+print moveZero([1,0,2,0,3,0,4,0,5,0])
