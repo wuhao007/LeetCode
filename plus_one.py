5feb285d7120f4e699fefb25ec07806388fd1469
Hao Wu <tohaowu@gmail.com>
2015-01-03 20:16:32 -0500

    miss lots of thing

--- plus_one.py
+++ plus_one.py
@@ -1,7 +1,18 @@
 def AddWithoutArithmetic(num1, num2):
     if num2 == 0: 
         return num1
-    sum = num1 ^ num2
-    carry = (num1 & num2) << 1
-    return AddWithoutArithmetic(sum,carry)
-print AddWithoutArithmetic(39, 1)
+    return AddWithoutArithmetic(num1 ^ num2, (num1 & num2) << 1)
+
+def plusOne(digits):
+    if digits == None or len(digits) == 0:
+        return digits
+
+    for i in range(len(digits) - 1, -1, -1):
+        if digits[i] == 9:
+            digits[i] = 0
+        else:
+            digits[i] = AddWithoutArithmetic(digits[i], 1)
+            return digits
+    ret = [0] * len(digits)
+    return ret
+print plusOne([3,9])
