7318ed718fbf8615d54c7ad274d80437a391f5e9
Hao Wu <tohaowu@gmail.com>
2015-05-21 08:18:30 -0700

    add a file that I don't know

---
+++ facebook/circle.py
@@ -0,0 +1,12 @@
+def best(lines):
+    nodes = []
+    n = len(lines)
+    for i in range(n):
+        cnt = 1
+        for j in range(n):
+            if 
+            cnt += 1
+    
+lines = [(-40, 40), (50, 130), (60, 120), (140, 220), (230, 310)] 
+best(lines)
+
